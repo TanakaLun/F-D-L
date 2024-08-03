1dfec6ed2d7fee628079e1e78bd8be95c3c7b649
TANAKAhikiko <115213949+TanakaLun@users.noreply.github.com>
2024-08-03 13:27:16 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,69 @@
+import os
+import requests
+import time
+import json
+import fgourl
+import user
+import coloredlogs
+import logging
+
+userIds = os.environ['userIds'].split(',')
+authKeys = os.environ['authKeys'].split(',')
+secretKeys = os.environ['secretKeys'].split(',')
+webhook_discord_url = os.environ['webhookDiscord']
+device_info = os.environ.get('DEVICE_INFO_SECRET')
+user_agent_2 = os.environ.get('USER_AGENT_SECRET_2')
+fate_region = 'JP'
+
+userNums = len(userIds)
+authKeyNums = len(authKeys)
+secretKeyNums = len(secretKeys)
+
+logger = logging.getLogger("FGO Daily Login")
+coloredlogs.install(fmt='%(asctime)s %(name)s %(levelname)s %(message)s')
+
+def get_latest_verCode():
+    endpoint = "https://raw.githubusercontent.com/DNNDHH/FGO-VerCode-extractor/JP/VerCode.json"
+    response = requests.get(endpoint).text
+    response_data = json.loads(response)
+
+    return response_data['verCode']
+    
+def get_latest_appver():
+    endpoint = "https://raw.githubusercontent.com/DNNDHH/FGO-VerCode-extractor/JP/VerCode.json"
+    response = requests.get(endpoint).text
+    response_data = json.loads(response)
+
+    return response_data['appVer']
+
+
+def main():
+    if userNums == authKeyNums and userNums == secretKeyNums:
+        fgourl.set_latest_assets()
+        for i in range(userNums):
+            try:
+                instance = user.user(userIds[i], authKeys[i], secretKeys[i])
+                time.sleep(3)
+                logger.info(f"\n ======================================== \n [+] 登录账号 \n ======================================== " )
+
+                time.sleep(1)
+                instance.topLogin_s()
+                time.sleep(2)
+                instance.topHome()
+                time.sleep(2)
+                instance.lq001()
+                instance.lq002()
+                time.sleep(2)
+                instance.buyBlueApple()
+                time.sleep(1)
+                instance.lq003()
+                time.sleep(1)
+                instance.drawFP()
+
+
+            except Exception as ex:
+                logger.error(ex)
+
+if __name__ == "__main__":
+    main()
+
